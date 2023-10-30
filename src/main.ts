9404ff7fb397c701ae934c7fd3a2328a34ca9a68
toledomg <toledomg@yahoo.com.br>
2023-10-30 13:35:59 -0300

    initial delete testes

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+//
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true }),
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { groups: ['transform'] },
+    }),
+  );
+
+  const PORT = process.env.PORT! || 3030;
+  app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT} 🧪🚀`);
+  });
+}
+bootstrap();
