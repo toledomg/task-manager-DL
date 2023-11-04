63fd9e3f83a9729cbda8689ed6fd970dcab9e096
toledomg <toledomg@yahoo.com.br>
2023-11-04 10:26:33 -0300

    refactor: add users.role no jwt e loginpayload type

--- src/modules/auth/dto/loginPayload-auth.dto.ts
+++ src/modules/auth/dto/loginPayload-auth.dto.ts
@@ -3,9 +3,11 @@ import { User } from 'src/modules/users/entities/user.entity';
 export class LoginPayload {
   sub: number;
   email: string;
+  role: string;
 
   constructor(user: User) {
     this.sub = user.id;
     this.email = user.email;
+    this.role = user.role;
   }
 }
